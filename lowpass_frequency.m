e40f53b0035ad2e3f618852b24ccd04d3657df3e
gimhafnu <60770867+gimhafnu@users.noreply.github.com>
2022-03-01 20:17:45 +0700

    Deleted comments & added some e-x-tra images

--- lowpass_frequency.m
+++ lowpass_frequency.m
@@ -1,6 +1,6 @@
 function filtered_image = lowpass_frequency(f, filter_name) % filter_name is %Ideal, Gaussian, Butterwoth        
     [P,Q] = size(f);
-
+    %% Fast Fourier Transform
     F = fft2(double(f));
     %% Make the H filter function
     D0 = 0.05*P;
@@ -26,19 +26,9 @@ function filtered_image = lowpass_frequency(f, filter_name) % filter_name is %Id
         n = 1; % default
         H = 1./(1 + (D./D0).^(2*n));
     end
-    %figure;imshow(H);title('LPF Mask');
-    
-    %g = zeros([P Q C], "uint8");
-    %% Fast Fourier Transform
-    %F = fft2(double(f(:,:,c)));
-    % disp(c);
-    
     %% Multiply F with the H filter
     G = H.*F;
     
     %% Get the real portion of the ifft of G
-    
-    %figure;imshow(real(ifft2(G)));title('AAAAA');
-    
     filtered_image = real(ifft2(G));
 end
--- lowpass_frequency_wrapper.m
+++ lowpass_frequency_wrapper.m
@@ -22,24 +22,14 @@ function filtered_image_wrapper = lowpass_frequency_wrapper(f, filter_name) % fi
         end
     end
     %%
-    %imshow(f);title('original image');
-    %figure; imshow(fp);title('padded image');
-
     for c = 1:C
         f_temp = fp(:,:,c);
         filtered_image = lowpass_frequency(f_temp, filter_name);
-        %figure, imshow(filtered_image); title('filtered_image');
-        %disp("filtered_image")
-        %disp(filtered_image(1:5,1:5))
         g(:,:,c) = filtered_image(:,:,1);
-        %disp("g(:,:,c)")
-        %disp(g(1:5,1:5,c))
     end
-    %figure, imshow((g)); title('gggg?');
 
     %% Undo padding
     g=g(1:M, 1:N, :); % Resize the image to undo padding
-    %figure, imshow(g); title('output image');
 
     %% Output
     filtered_image_wrapper = g;
